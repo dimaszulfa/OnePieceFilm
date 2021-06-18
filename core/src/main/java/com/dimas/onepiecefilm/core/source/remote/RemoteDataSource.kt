4d93d129a516c4d021a6c9d4d6d5d767a6531418
dimaszulfa <dimaszulfa0@gmail.com>
2021-06-18 10:10:39 +0700

    add obfuscation

--- core/src/main/java/com/dimas/onepiecefilm/core/source/remote/RemoteDataSource.kt
+++ core/src/main/java/com/dimas/onepiecefilm/core/source/remote/RemoteDataSource.kt
@@ -15,6 +15,7 @@ class RemoteDataSource(private val apiService: ApiService){
         return flow {
             try {
                 val response = apiService.getList()
+                Log.d("RemoteDataSource","Isi dataList ${response.results}")
                 val dataList = response.results
                 if (dataList.isNotEmpty()){
                     emit(ApiResponse.Success(dataList))
